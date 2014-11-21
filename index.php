5c38e1c3832623b0b9f987d80c91771db45b3715
AlexShearcroft <alex@stormcreative.co.uk>
2014-11-21 14:44:25 +0000

    helping a friend

---
+++ index.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<!--[if IE 8]><html class="ie8" dir="ltr" lang="en"><![endif]-->
+<!--[if IE 9]><html class="ie9" dir="ltr" lang="en"><![endif]-->
+<!--[if gt IE 9]><!--> <html dir="ltr" lang="en"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta name="author" content="" />
+        <title></title>
+        <meta name="description" content="">
+        <meta name="robots" content="index, follow">
+        <meta name="revisit-after" content="7 days"/>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+        <link rel="icon" href="favicon.ico" type="image/x-icon">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <link rel="stylesheet" href="assets/styles/layout.css?v=1">
+        <!--[if IE 8]>
+        <link rel="stylesheet" href="assets/styles/nomq.css?v=1">
+        <![endif]-->
+        <script src="assets/scripts/utils/modernizr.min.js"></script>
+    </head>
+    <body>
+        <div class="wrapper">
+			<header class="header">
+				<div class="header-content">
+					<h1 class="header__title">Lorem ipsum dolar sit</h1>
+				</div>
+			</header>
+			<div class="content">
+				<div class="container box-wrap">
+					<div class="box">
+						<a href="#" class="box-link">
+							<img src="assets/images/image1.jpg" width="320" height="320" alt="" class="box__img" />
+							<div class="box-content box-content--top">
+								<h2 class="box__title">Lorem ipsum dolar sit</h2>
+								<p class="box__text">Lorem ipsum dolar sit. Lorem ipsum dolar sit. Lorem ipsum dolar sit.</p>
+							</div>
+						</a>
+					</div>
+					<div class="box">
+						<a href="#" class="box-link">
+							<img src="assets/images/image1.jpg" width="320" height="320" alt="" class="box__img" />
+							<div class="box-content box-content--bottom">
+								<h2 class="box__title">Lorem ipsum dolar sit</h2>
+								<p class="box__text">Lorem ipsum dolar sit. Lorem ipsum dolar sit. Lorem ipsum dolar sit.</p>
+							</div>
+						</a>
+					</div>
+					<div class="box">
+						<a href="#" class="box-link">
+							<img src="assets/images/image1.jpg" width="320" height="320" alt="" class="box__img" />
+							<div class="box-content box-content--middle">
+								<h2 class="box__title">Lorem ipsum dolar sit</h2>
+								<p class="box__text">Lorem ipsum dolar sit. Lorem ipsum dolar sit. Lorem ipsum dolar sit.</p>
+							</div>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
